d18cca9e67dd40f326b271e9a92880d6afde5687
heitor <heitordesousa@gmail.com>
2019-06-22 15:30:52 -0300

    fix para o deploy

---
+++ dist/Tests/RedisIntegration.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+describe('Redis Integration', () => {
+    it('should add new key-value pair', () => { });
+    it('should throw error when adding same key', () => { });
+    it('should add new key-value pair and retrieve it', () => { });
+});
