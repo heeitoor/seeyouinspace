ec9051f4c5c6d6bd924da63fd643b4e1a187d04c
heitor <heitordesousa@gmail.com>
2019-06-24 08:20:45 -0300

    teste com cron job

--- src/Index.ts
+++ src/Index.ts
@@ -7,10 +7,20 @@ import AuthMiddleware from './Middlewares/Auth';
 import { MiddlewareOrder } from './Engine/Utils/Enums';
 import TokenRouter from './Routes/Token';
 
-new Server(
-  [
-    container.get<RedisRouter>(types.RedisRouter),
-    container.get<TokenRouter>(types.TokenRouter),
-  ],
-  [new AuthMiddleware(MiddlewareOrder.Begin)],
-).start(process.env.PORT);
+import cron from 'cron';
+import moment from 'moment';
+
+const cronJob = new cron.CronJob('*/1 * * * *', () => {
+  console.log('moment()', moment());
+  console.log('moment().utc()', moment().utc());
+});
+
+cronJob.start();
+
+// new Server(
+//   [
+//     container.get<RedisRouter>(types.RedisRouter),
+//     container.get<TokenRouter>(types.TokenRouter),
+//   ],
+//   [new AuthMiddleware(MiddlewareOrder.Begin)],
+// ).start(process.env.PORT);
